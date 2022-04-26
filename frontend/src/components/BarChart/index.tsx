7df9b7f2301a3a25896ebae77432c1ca4793adcd
Ikari ♡ <83562216+soninhoo@users.noreply.github.com>
2022-04-26 16:20:48 -0300

    mudei os nomes do componnents BarChart

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -12,7 +12,7 @@ const BarChart = () => {
     
     const mockData = {
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+            categories: ['Mayle', 'Wesley', 'Guilherme', 'Oliver', 'Paulo']
         },
         series: [
             {
